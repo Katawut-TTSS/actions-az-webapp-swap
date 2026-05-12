39c182e41dd41149bb1d42e57589c6c8c11bff11
KT-Dev <ktdev.in.th@gmail.com>
2026-05-12 09:44:17 +0700

    remove

--- .github/workflows/config/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const jsonPath = path.resolve(process.argv[2]);
-const json = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
-const outputName = 'deployment-matrix';
-
-console.log(json);
-console.log(`::set-output name=${outputName}::${JSON.stringify(json)}`);
